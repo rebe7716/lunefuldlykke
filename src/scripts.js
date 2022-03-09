545f9e334ebf1ce1da470c1fe8d9ff0a05dc9887
Stefan Grage <stefangrage@Stefans-iMac-2.local>
2022-03-09 08:10:00 +0100

    bygget videre på sitet med flere templates, bl.a. shop og produkt.html

--- src/scripts.js
+++ src/scripts.js
@@ -13,3 +13,22 @@ const closeMenu = () => {
     document.getElementById("mobile-menu").classList.add("hidden");
 };
 // Navigation slut
+
+function showFilters() {
+    var fSection = document.getElementById("filterSection");
+    if (fSection.classList.contains("hidden")) {
+        fSection.classList.remove("hidden");
+        fSection.classList.add("block");
+    } else {
+        fSection.classList.add("hidden");
+    }
+}
+
+function applyFilters() {
+    document.querySelectorAll("input[type=checkbox]").forEach((el) => (el.checked = false));
+}
+
+function closeFilterSection() {
+    var fSection = document.getElementById("filterSection");
+    fSection.classList.add("hidden");
+}
